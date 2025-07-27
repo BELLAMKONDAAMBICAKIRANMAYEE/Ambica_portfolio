43b4fd6af04338510fc893a23ea3efe3ad991bc6
BELLAMKONDAAMBICAKIRANMAYEE <ambica930@gmail.com>
2025-07-27 18:50:08 +0530

    first commit

---
+++ utils/data/educations.js
@@ -0,0 +1,24 @@
+export const educations = [
+ {
+  id: 1,
+title: "Bachelor of Technology (University Gold Medalist)",
+duration: "2018 - 2022",
+CGPA: "8.8",
+institution: "Jawaharlal Nehru Technological University, College of Engineering, Kalikiri",
+
+ },
+  {
+    id: 2,
+    title: "Board of Intermediate Education, Andhra Pradesh",
+    duration: "2016 - 2018",
+    CGPA:"9.7",
+    institution: "Sarada jr college",
+  },
+  {
+    id: 3,
+    title: "Secondary School Certificate",
+    duration: "2011 - 2016",
+      CGPA:"9.7",
+    institution: "Sri chaitanya schools",
+  }
+]
---
+++ utils/data/experience.js
@@ -0,0 +1,20 @@
+export const experiences = [
+  {
+    id: 1,
+    title: 'FRONTEND DEVELOPER  ',
+    company: "CODEGNAN IT SOLUTIONS PRIVATE LI",
+    duration: "(April 2023 - Present)"
+  },
+  {
+    id: 2,
+    title: "FullStack Developer",
+    company: "freelance",
+    duration: "(Jun 2024 - Present)"
+  },
+  {
+    id: 3,
+    title: "Self Employed",
+    company: "Code and build something in everyday.",
+    duration: "(Jan 2022 - Present)"
+  }
+]
